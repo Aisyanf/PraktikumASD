6f7f95aec9c2dc2fa2404f11cd3dc8f1d429b991
Aisyanf <an.fardani@gmail.com>
2026-05-15 12:13:06 +0700

    Praktikum1

---
+++ Jobsheet12/DoubleLinkedList02.java
@@ -0,0 +1,161 @@
+package Jobsheet12;
+
+import java.util.Scanner;
+
+public class DoubleLinkedList02 {
+    Node02 head;
+    Node02 tail;
+
+    public DoubleLinkedList02() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public static Mahasiswa02 inputMahasiswa(Scanner scan) {
+        System.out.print("Masukkan NIM   : ");
+        String nim = scan.nextLine();
+        System.out.print("Masukkan Nama  : ");
+        String nama = scan.nextLine();
+        System.out.print("Masukkan Kelas : ");
+        String kelas = scan.nextLine();
+        System.out.print("Masukkan IPK   : ");
+        double ipk = scan.nextDouble();
+        scan.nextLine();
+
+        return new Mahasiswa02(nim, nama, kelas, ipk);
+    }
+
+    public void removeFirst() {
+    }
+
+    public void removeLast() {
+    }
+
+    public void addFirst(Mahasiswa02 data) {
+        Node02 newNode = new Node02(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa02 data) {
+        Node02 newNode = new Node02(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa02 data) {
+        Node02 current = head;
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println("Data dengan NIM : " + keyNim + " tidak ditemukan");
+            return;
+        }
+
+        Node02 newNode = new Node02(data);
+
+        // jika current adalah tail, node baru ditambahkan di akhir
+        if (current == tail) {
+            newNode.prev = current;
+            current.next = newNode;
+            tail = newNode;
+        } else { // node baru disisipkan di tengah
+            newNode.prev = current;
+            newNode.next = current.next;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+
+        System.out.println("Data berhasil disisipkan setelah NIM : " + keyNim);
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong");
+            return;
+        }
+
+        Node02 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DoubleLinkedList02 list = new DoubleLinkedList02();
+        int pilihan;
+
+        do {
+            System.out.println("\n==== MENU DOUBLE LINKED LIST ====");
+            System.out.println("1. Tambah data di awal");
+            System.out.println("2. Tambah data di akhir");
+            System.out.println("3. Sisipkan data di tengah (setelah NIM)");
+            System.out.println("4. Hapus data di awal");
+            System.out.println("5. Hapus data di akhir");
+            System.out.println("6. Tampilkan data");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu : ");
+            pilihan = scan.nextInt();
+            scan.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    Mahasiswa02 mhsAwal = inputMahasiswa(scan);
+                    list.addFirst(mhsAwal);
+                    break;
+
+                case 2:
+                    Mahasiswa02 mhsAkhir = inputMahasiswa(scan);
+                    list.addLast(mhsAkhir);
+                    break;
+
+                case 3:
+                    System.out.print("Masukkan NIM yang dicari : ");
+                    String keyNim = scan.nextLine();
+                    System.out.println("Masukkan data baru: ");
+                    Mahasiswa02 dataBaru = inputMahasiswa(scan);
+                    list.insertAfter(keyNim, dataBaru);
+                    break;
+
+                case 4:
+                    list.removeFirst();
+                    break;
+
+                case 5:
+                    list.removeLast();
+                    break;
+
+                case 6:
+                    list.print();
+                    break;
+
+                case 0:
+                    System.out.println("Program selesai.");
+                    break;
+
+                default:
+                    System.out.println("Menu tidak valid.");
+            }
+
+        } while (pilihan != 0);
+
+        scan.close();
+    }
+}
